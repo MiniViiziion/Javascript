3d36e1a8736da81041d903563bfb1e7536c8da1f
MiniViiziion <123436547+MiniViiziion@users.noreply.github.com>
2023-03-26 20:19:23 -0500

    Add files via upload

---
+++ Javascript/Module1.js
@@ -0,0 +1,8 @@
+﻿console.log('Brec Buster');
+console.log('2002');
+console.log(2002);
+console.log("Brec", "Buster", "(2002)");
+console.log("Brec");
+console.log("Buster");
+console.log("(2002)");
+console.log("Brec " + " Buster " + " (2002) ");
